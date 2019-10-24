a3799436c74fbdb1aedd3714ce4006a7b367806b
Cheng.Yang4 <cheng.yang4@geely.com>
2019-10-24 09:53:39 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <scroll-select/>
+  </div>
+</template>
+
+<script>
+import scrollSelect from './components/scrollSelect'
+
+export default {
+  name: 'App',
+  components: {
+    scrollSelect
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/scrollSelect.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    滚轮选择器
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'scrollSelect',
+
+}
+</script>
+
+<style>
+
+</style>
